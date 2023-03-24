b9e764d262112addfb95a8b3d2395aaec046bec2
Manoj Prabaker aka MJ <120574373+manojprabaker@users.noreply.github.com>
2023-03-24 11:52:20 +0530

    add

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+const person = [
+  {
+    first_name: 'Mike',
+    last_name: 'Sheridan',
+    age: 30
+  },
+  {
+    first_name: 'Tim',
+    last_name: 'Lee',
+    age: 45
+  },
+  {
+    first_name: 'John',
+    last_name: 'Carte',
+    age: 25
+  }
+];
+
+let personDetail=users.map(function(f)
+{
+		return f.last_name;	
+	
+});
+console.log(personDetail);
+
+
+
+
+
